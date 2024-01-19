6334f61dcbe8a5286ec8e060da13537bb5ea4c99
andreerosaa <andrefsantosrosa@gmail.com>
2024-01-19 21:10:30 +0000

    initial styling

---
+++ src/app/modules/album-details/album-details-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AlbumListComponent } from '../album-list/album-list-component/album-list.component';
+
+const routes: Routes = [
+  {
+    path: 'list',
+    component: AlbumListComponent,
+    loadChildren: () =>
+      import('../album-list/album-list.module').then((m) => m.AlbumListModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AlbumDetailsRoutingModule {}
--- src/app/modules/album-details/album-details.module.ts
+++ src/app/modules/album-details/album-details.module.ts
@@ -4,9 +4,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { AlbumDetailsComponent } from './album-details-component/album-details.component';
 import { FavouriteIconComponent } from './favourite-icon-component/favourite-icon.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { AlbumDetailsRoutingModule } from './album-details-routing.module';
 
 @NgModule({
   declarations: [AlbumDetailsComponent, FavouriteIconComponent],
-  imports: [CommonModule, MatIconModule, MatProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    AlbumDetailsRoutingModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class AlbumDetailsModule {}
